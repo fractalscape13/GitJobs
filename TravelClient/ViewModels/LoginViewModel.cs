fe9b7c8aa798e4be508979647eafd0b36d17673e
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-01 16:34:43 -0700

    create viewmodels for login and registration
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>

---
+++ TravelClient/ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelClient.ViewModels
+{
+  public class LoginViewModel
+  {
+    public string UserName { get; set; }
+    public string Password { get; set; }
+  }
+}
