fd4ecc4c16470f3314c77ec59df3a160a24460b0
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-01 10:43:40 -0700

    create methods for review model and apihelper
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

--- TravelClient/Models/ApiHelper.cs
+++ TravelClient/Models/ApiHelper.cs
@@ -12,5 +12,40 @@ namespace TravelClient.Models
       var response = await client.ExecuteTaskAsync(request);
       return response.Content;
     }
+
+    public static async Task<string> Get(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5004/api");
+      RestRequest request = new RestRequest($"reviews/{id}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+
+    // do we need to add a header for authorization?? 
+    public static async Task Post(string newReview)
+    {
+      RestClient client = new RestClient("http://localhost:5004/api");
+      RestRequest request = new RestRequest($"reviews", Method.POST);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newReview);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task Put(int id, string newReview)
+    {
+      RestClient client = new RestClient("http://localhost:5004/api");
+      RestRequest request = new RestRequest($"reviews/{id}",Method.PUT);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newReview);
+      var response = await client.ExecuteTaskAsync(request);
+    }
+
+    public static async Task Delete(int id)
+    {
+      RestClient client = new RestClient("http://localhost:5004/api");
+      RestRequest request = new RestRequest($"reviews/{id}", Method.DELETE);
+      request.AddHeader("Content-Type", "application/json");
+      var response = await client.ExecuteTaskAsync(request);
+    }
   }
 }
--- TravelClient/Models/Review.cs
+++ TravelClient/Models/Review.cs
@@ -27,6 +27,32 @@ namespace TravelClient.Models
       return reviewList;
     }
 
-    
+    public static Review GetDetails(int id)
+    {
+      var apiCallTask = ApiHelper.Get(id);
+      var result = apiCallTask.Result;
+
+      JObject jsonResponse =JsonConvert.DeserializeObject<JObject>(result);
+      Review review = JsonConvert.DeserializeObject<Review>(jsonResponse.ToString());
+
+      return review;
+    }
+
+    public static void Post(Review review)
+    {
+      string jsonReview = JsonConvert.SerializeObject(review);
+      var apiCallTask = ApiHelper.Post(jsonReview);
+    }
+
+    public static void Put(Review review)
+    {
+      string jsonReview = JsonConvert.SerializeObject(review);
+      var apiCallTask = ApiHelper.Put(review.ReviewId, jsonReview);
+    }
+
+    public static void Delete(int id)
+    {
+      var apiCallTask = ApiHelper.Delete(id);
+    }
   }
 }
