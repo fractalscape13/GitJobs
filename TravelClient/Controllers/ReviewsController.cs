a13d61a1e1d2104013e045fdddd0e7bd463f0372
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-01 10:42:37 -0700

    add full crud methods to reviewscontroller
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Jonathan Carlos <jonathancarlos21@hotmail.com>
    Co-authored-by: Kristina Hengster Tintor <hengster.tintor@gmail.com>

---
+++ TravelClient/Controllers/ReviewsController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelClient.Models;
+
+namespace TravelClient.Controllers
+{
+  public class ReviewsController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allReviews = Review.GetAll();
+      return View(allReviews);
+    }
+
+    [HttpPost]
+    public IActionResult Index(Review review)
+    {
+      Review.Post(review);
+      return RedirectToAction("Index");
+    }
+
+    public IActionResult Details(int id)
+    {
+      var review = Review.GetDetails(id);
+      return View(review);
+    }
+
+    public IActionResult Edit(int id)
+    {
+      var review = Review.GetDetails(id);
+      return View(review);
+    }
+
+    [HttpPost]
+    public IActionResult Details(int id, Review review)
+    {
+      review.ReviewId = id;
+      Review.Put(review);
+      return RedirectToAction("Details", id);
+    }
+
+    public IActionResult Delete(int id)
+    {
+      Review.Delete(id);
+      return RedirectToAction("Index");
+    }
+  }
+}
