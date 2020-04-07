90aeb0324c1810d2bdecb1d8e894b723f889d8bb
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-07 16:26:20 -0700

    update create method to create new job object and save to database
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- GitJobs/Controllers/JobsController.cs
+++ GitJobs/Controllers/JobsController.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using GitJobs.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System;
 
 namespace GitJobs.Controllers
 {
   [Authorize]
   public class JobsController : Controller
   {
+    private readonly GitJobsContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public JobsController(UserManager<ApplicationUser> userManager, GitJobsContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
     [AllowAnonymous]
     public IActionResult Index()
     {
@@ -14,17 +27,23 @@ namespace GitJobs.Controllers
       return View(allJobs);
     }
 
-    public IActionResult Create()
-    {
-      return View();
-    }
-
-    // [HttpPost]
-    // public IActionResult Create(Job job)
+    // public IActionResult Create()
     // {
-    //   Job.Post(job);
-    //   return RedirectToAction("Index");
+       
+    //   return View();
     // }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(string desc, string loc, string tit, string ur)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      Job newJob = new Job(desc, loc, tit, ur);
+      newJob.User = currentUser;
+      _db.Jobs.Add(newJob);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
 
     [AllowAnonymous]
     public IActionResult Search(string description, string location, string title)
@@ -39,6 +58,14 @@ namespace GitJobs.Controllers
       var job = Job.GetDetails(id);
       return View(job);
     }
+
+    // [HttpPost]
+    // public ActionResult Search(string search)
+    // {
+    //   List<Job> model = _db.Jobs.Where(job => (job.Description.Contains(search)) || (job.Title.Contains(search)) || (job.Location.Contains(search))).ToList();
+    //   ViewBag.Model = model;
+    //   return RedirectToAction("Index");
+    // }
 
     // public IActionResult Edit(int id)
     // {
