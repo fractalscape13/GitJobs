5ca26c69ea7c3bb54b5720e8d2a9dc841aebb3db
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-07 16:05:17 -0700

    add migration to add userid field to job database
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- GitJobs/Migrations/20200407225523_AddAppUser.Designer.cs
+++ GitJobs/Migrations/20200407225523_AddAppUser.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GitJobs.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GitJobs.Migrations
 {
     [DbContext(typeof(GitJobsContext))]
-    partial class GitJobsContextModelSnapshot : ModelSnapshot
+    [Migration("20200407225523_AddAppUser")]
+    partial class AddAppUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GitJobs/Migrations/20200407225523_AddAppUser.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GitJobs.Migrations
+{
+    public partial class AddAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Jobs",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jobs_UserId",
+                table: "Jobs",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Jobs_AspNetUsers_UserId",
+                table: "Jobs",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Jobs_AspNetUsers_UserId",
+                table: "Jobs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Jobs_UserId",
+                table: "Jobs");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Jobs",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
