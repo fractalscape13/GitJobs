4ca72b3286157ef3a40bc9c70745f7a578833cad
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-08 14:06:02 -0700

    add migration for 2 additional properties of job model
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

--- GitJobs/Migrations/20200408210344_UpdateProperties.Designer.cs
+++ GitJobs/Migrations/20200408210344_UpdateProperties.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GitJobs.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GitJobs.Migrations
 {
     [DbContext(typeof(GitJobsContext))]
-    partial class GitJobsContextModelSnapshot : ModelSnapshot
+    [Migration("20200408210344_UpdateProperties")]
+    partial class UpdateProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
