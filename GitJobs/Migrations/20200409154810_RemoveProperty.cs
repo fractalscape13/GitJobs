d821b29e1282b33259964538732c5c885acb148e
Joseph Wangemann <fractalscape13@gmail.com>
2020-04-09 08:51:06 -0700

    add new migration to remove description property from job model
    Co-authored-by: Zakkrey Short <zakkreyjshort@gmail.com>
    Co-authored-by: Hristo Dzhambov <dzhambov@hotmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

---
+++ GitJobs/Migrations/20200409154810_RemoveProperty.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GitJobs.Migrations
+{
+    public partial class RemoveProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Jobs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Jobs",
+                nullable: true);
+        }
+    }
+}
